3d99095ea92c7698071b6b19a07cac852a224515
Nazar Bagrii <nazarbagrii@Sombra-L323.local>
2023-04-06 18:51:41 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'promo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'promotionT';
+}
